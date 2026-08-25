ab41eedb288c30be9eb21629fbd6c5ab6e1cff11
Richard Hanly <richardrhanly@gmail.com>
2026-08-25 11:48:47 -0500

    Add CloudWatch agent IAM permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,6 +114,11 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 resource "aws_iam_instance_profile" "ec2" {
   name = "${var.project_name}-ec2-profile"
   role = aws_iam_role.ec2.name
